37da8560087af8bba6dd815be5f6948db8a81b14
Pavel Myasnov <wormaster5@gmail.com>
2019-11-06 19:23:28 +0300

    created edit and delete actions for news in admin panel fixed problem with data type input (it was given text type)

--- module/Admin/src/Admin/Controller/NewsController.php
+++ module/Admin/src/Admin/Controller/NewsController.php
@@ -81,4 +81,92 @@ class NewsController extends BaseController
 
         return $this->redirect()->toRoute('admin/news');
     }
+
+    public function editAction()
+    {
+        $message = $status = '';
+        $em = $this->getEntityManager();
+        $form = new NewsAddForm($em);
+
+        $id = (int) $this->params()->fromRoute('id', 0);
+        $news = $em->find('News\Entity\News', $id);
+
+        if(empty($news)){
+            $message = 'Новость не найдена';
+            $status = 'error';
+            $this->flashMessenger()
+                ->setNamespace($status)
+                ->addMessage($message);
+
+            return $this->redirect()->toRoute('admin/news');
+        }
+
+        $form->setHydrator(new DoctrineHydrator($em, '\News'));
+        $form->bind($news);
+        $form->get('created')->setValue($news->getCreated()->format('m/d/Y H:i'));
+        $form->get('updated')->setValue($news->getUpdated()->format('m/d/Y H:i'));
+        $request = $this->getRequest();
+
+        if ($request->isPost())
+        {
+            $data = $request->getPost();
+            $form->setData($data);
+            $datetime = new \DateTime("now");
+
+            if ($form->isValid()){
+
+                $news->setUpdated($datetime);
+                $em->persist($news);
+                $em->flush();
+
+                $status = 'success';
+                $message = 'Новость обновлена';
+            } else {
+                $status = 'error';
+                $message = 'Ошибка параметров';
+                foreach ($form->getInputFilter()->getInvalidInput() as $errors) {
+                    foreach ($errors->getMessages() as $error) {
+                        $message .= ' ' . $error;
+                    }
+                }
+            }
+        } else {
+            return array('form' => $form, 'id' => $id);
+        }
+
+        if ($message)
+        {
+            $this->flashMessenger()
+                ->setNamespace($status)
+                ->addMessage($message);
+        }
+
+        return $this->redirect()->toRoute('admin/news');
+    }
+
+    public function deleteAction()
+    {
+        $id = (int) $this->params()->fromRoute('id', 0);
+        $em = $this->getEntityManager();
+
+        $status = 'success';
+        $message = 'Запись удалена';
+
+        try {
+            $repository = $em->getRepository('News\Entity\News');
+            $news = $repository->find($id);
+            $em->remove($news);
+            $em->flush();
+        }
+        catch (\Exception $ex) {
+            $status = 'error';
+            $message = 'Ошибка удаления записи: ' . $ex->getMessage();
+        }
+
+        $this->flashMessenger()
+            ->setNamespace($status)
+            ->addMessage($message);
+
+        return $this->redirect()->toRoute('admin/news');
+    }
 }
